8a18a589e6a1658f7b3dca62b0776a8bb6501de3
= <=>
2018-03-13 01:01:27 +0530

    Added Static Directory

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 set static path
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res){
    res.redirect('/public');
